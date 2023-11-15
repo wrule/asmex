8e812541497f72ed2e3eea692bb2be8ff56b7869
jimao <jimao@jimaodeMacBook-Pro.local>
2023-11-15 14:21:34 +0800

    删除不需要的和玉

--- dev/index.ts
+++ dev/index.ts
@@ -1,11 +1,9 @@
-import { ethers } from 'hardhat';
-import { A, X } from '../typechain-types';
 import { context, deployAllContract, getAllContract, init, meta } from './utils';
 
 async function main() {
   await meta();
   // await deployAllContract();
-  context(({ A, X }) => {
+  context(({ X }) => {
 
   });
 }
