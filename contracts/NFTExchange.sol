cf9f7c0ffb6edb4ba1961b5551e88e5be9cec342
jimao <jimao@jimaodeMacBook-Pro.local>
2023-11-15 14:34:30 +0800

    提交代码

--- contracts/NFTExchange.sol
+++ contracts/NFTExchange.sol
@@ -5,12 +5,15 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 contract NFTExchange {
-
   function limitSell(IERC721 erc721, uint256 tokenId) external {
 
   }
 
   function targetBuy(IERC721 erc721, uint256 tokenId) external payable {
+
+  }
+
+  function list(IERC721 erc721, uint256 pageNum, uint8 pageSize) external {
 
   }
 }
