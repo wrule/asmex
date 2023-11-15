2dd71513704fcba2c3944991e6489596d52f35fb
jimao <jimao@jimaodeMacBook-Pro.local>
2023-11-15 14:40:46 +0800

    提交代码

--- contracts/NFTExchange.sol
+++ contracts/NFTExchange.sol
@@ -4,12 +4,31 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
+struct Order {
+  uint256 tokenId;
+  uint256 amount;
+  address tokenAddress;
+}
+
 contract NFTExchange {
-  function limitSell(IERC721 erc721, uint256 tokenId) external {
+  function limitSell(
+    IERC721 erc721,
+    uint256 tokenId,
+    uint256 amount,
+    address tokenAddress
+    ) external {
 
   }
 
-  function targetBuy(IERC721 erc721, uint256 tokenId) external payable {
+  function targetBuyETH(IERC721 erc721, uint256 tokenId) external payable {
+
+  }
+
+  function targetBuyERC20(IERC721 erc721, uint256 tokenId) external {
+
+  }
+
+  function targetBuyERC721(IERC721 erc721, uint256 tokenId) external {
 
   }
 
