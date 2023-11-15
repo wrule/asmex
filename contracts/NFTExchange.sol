c953aa524ba9eaa4ecaccfeb7df0c3e760cb1281
jimao <jimao@jimaodeMacBook-Pro.local>
2023-11-15 16:38:49 +0800

    提交代码

--- contracts/NFTExchange.sol
+++ contracts/NFTExchange.sol
@@ -13,13 +13,29 @@ struct Order {
 
 contract NFTExchange {
   mapping(IERC721 => mapping(uint256 => Order)) orders;
+  mapping(IERC721 => Order[]) orderView;
 
   function limitSellETH(
     IERC721 erc721,
     uint256 tokenId,
     uint256 ethPrice
   ) external {
+    Order storage order = orders[erc721][tokenId];
+    order.owner = msg.sender;
+    order.price = ethPrice;
+    order.erc20Address = IERC20(address(0));
+  }
 
+  function limitSellERC20(
+    IERC721 erc721,
+    uint256 tokenId,
+    IERC20 erc20,
+    uint256 erc20Price
+  ) external {
+    Order storage order = orders[erc721][tokenId];
+    order.owner = msg.sender;
+    order.price = erc20Price;
+    order.erc20Address = erc20;
   }
 
   function targetBuyETH(IERC721 erc721, uint256 tokenId) external payable {
